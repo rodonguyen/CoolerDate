2a6f504980086ab9c78f2452335963056d3de387
Rodo <rodonguyendd@gmail.com>
2023-01-12 16:48:47 +1000

    Complete test for coolerDate/code

--- routes/coolerDate.code.js
+++ routes/coolerDate.code.js
@@ -3,17 +3,26 @@ const router = express.Router();
 const Code = require("../models/coolerDate.code");
 
 // Finding an entry
-router.post('/find', getEntry, (req, res) => {
-  if (res.found)  res.status(201).json({message: "Entry exists", found: true, entry: res.entry})
-  else            res.status(201).json({message: "Entry does not exist", found: false, request: req.body})
-})
-
+router.post("/find", getEntry, (req, res) => {
+  if (res.found)
+    res
+      .status(201)
+      .json({ message: "Entry exists", found: true, entry: res.entry });
+  else
+    res
+      .status(201)
+      .json({
+        message: "Entry does not exist",
+        found: false,
+        request: req.body,
+      });
+});
 
 // Creating new coolerDate code
 router.post("/add", getEntry, async (req, res) => {
   if (res.found) {
-    res.status(201).json({message: "Entry exists, do nothing"})
-    return
+    res.status(201).json({ message: "Entry exists, do nothing" });
+    return;
   }
   try {
     const newCode = await Code.create({
@@ -22,67 +31,64 @@ router.post("/add", getEntry, async (req, res) => {
     });
     res.status(201).json(newCode);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({ error: err.message });
   }
 });
 
 // Updating One
-router.patch("/pathFirstAccessTime", async (req, res) => {
+router.patch("/patchFirstAccessTime", async (req, res) => {
   try {
     const addedTimeEntry = await Code.findOneAndUpdate(
       {
         username: req.body.username,
         code: req.body.code,
       },
-      { firstAccessTime: new Date()}
-    );
+      { firstAccessTime: new Date() }
+    ).then((res) => {
+      console.log(res)
+      if (res === null) return { message: "Entry does not exist, do nothing" };
+      return res
+    });
     res.status(201).json(addedTimeEntry);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({ error: err.message });
   }
 });
 
-
 // Deleting one
-router.delete('/deleteOne', getEntry, async (req, res) => {
+router.delete("/deleteOne", getEntry, async (req, res) => {
   if (!res.found) {
-    res.status(201).json({message: "Entry does not exist, do nothing"})
-    return
+    res.status(201).json({ message: "Entry does not exist, do nothing" });
+    return;
   }
   try {
-    await res.entry.remove()
-    res.json({ message: 'Deleted Entry', entry: res.entry })
+    await res.entry.remove();
+    res.json({ message: "Deleted Entry", entry: res.entry });
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ error: err.message });
   }
-})
-
-
-
+});
 
 async function getEntry(req, res, next) {
-  let entry
+  let entry;
 
   try {
     entry = await Code.findOne({
       username: req.body.username,
-      code: req.body.code
-    })
+      code: req.body.code,
+    });
   } catch (err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
 
   // console.log(entry)
   if (!entry) {
-    res.found = false
+    res.found = false;
+  } else {
+    res.found = true;
+    res.entry = entry;
   }
-  else {
-    res.found = true
-    res.entry = entry
-  }
-  next()
+  next();
 }
-
-
 
 module.exports = router;
